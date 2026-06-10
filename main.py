574805425bed6e510dc30be7a9786889c6b49fbd
Nguyen Tran Khanh Phung <phung.nguyentran05@gmail.com>
2026-06-10 15:33:48 +0700

    feat: dev A tinh gpa theo tuyen tinh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def tinh_diem_gpa(diem_so):
-    # TODO: Cần cập nhật logic quy đổi sang hệ 4
-    return 0.0
+    # Công thức tuyến tính đơn giản của Dev A
+    return round((diem_so / 10) * 4, 2)
 
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
